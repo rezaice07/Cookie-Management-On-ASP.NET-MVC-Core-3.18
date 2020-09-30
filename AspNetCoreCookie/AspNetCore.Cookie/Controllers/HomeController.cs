1d3211035a5e5b69df71b5efb0b2790877720ca5
Md. Rezazoyanul Islam <rezaice07@gmail.com>
2020-10-01 01:35:36 +0600

    Cookie Management on ASP.NET Core
    Cookie Management on ASP.NET Core Init

---
+++ AspNetCoreCookie/AspNetCore.Cookie/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AspNetCore.Cookie.Models;
+using AspNetCore.Cookie.TrackingServices;
+
+namespace AspNetCore.Cookie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IAddToCartEventTraking _addToCartEventTraking;
+
+        public HomeController(ILogger<HomeController> logger,
+            IAddToCartEventTraking addToCartEventTraking
+            )
+        {
+            _logger = logger;
+            this._addToCartEventTraking = addToCartEventTraking;
+        }
+
+        public IActionResult Index()
+        {
+            var patientLabTestKey = "QWR45-464654-HGE9879_jjlj45";
+
+            //track or set value into the cookie
+            _addToCartEventTraking.TrackToAddCartIdentifier(patientLabTestKey);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            var patientLabTestKey = _addToCartEventTraking.GetCartIdentifier();
+
+            ViewBag.PatientLabTestKey = patientLabTestKey;
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AspNetCoreCookie/AspNetCore.Cookie/TrackingServices/AddToCartEventTraking.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Cookie.TrackingServices
+{
+    public class AddToCartEventTraking: IAddToCartEventTraking
+    {
+        //private members
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly IConfiguration configuration;  
+
+        public AddToCartEventTraking(IHttpContextAccessor httpContextAccessor,
+            IConfiguration configuration)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+            this.configuration = configuration;
+        }
+
+
+        public string GetCartIdentifier()
+        {
+            var cartIdentifier = "";
+            try
+            {
+                //read cookie value from appsettings.js
+                var trackingCookieName = configuration["AspNetCore3.1.Cookie.AddToCartTraking"];
+
+                var cartIdentifierCookie = httpContextAccessor.HttpContext.Request.Cookies[trackingCookieName];
+
+                cartIdentifier = cartIdentifierCookie != null ? cartIdentifierCookie : string.Empty;
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+            return cartIdentifier;
+        }
+
+        public void TrackToAddCartIdentifier(string cartIdenfier)
+        {
+            var cookieExpireDays = 30;
+
+            try
+            {
+                //read cookie value from appsettings.js
+                var trackingCookieName = configuration["AspNetCore3.1.Cookie.AddToCartTraking"];
+
+                //lets remove first this identifier
+                RemoveCookie(trackingCookieName);
+
+                //let's set cookie info
+                SetCookie(trackingCookieName, cartIdenfier, cookieExpireDays);
+               
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+
+        #region Private Methods
+
+
+        private void SetCookie(string cookieKey,string cookieValue, int? expiretime)
+        {
+            CookieOptions options = new CookieOptions();
+
+            options.Expires = DateTime.Now.AddDays(7);
+
+            if (expiretime.HasValue)
+                options.Expires = DateTime.Now.AddDays(expiretime.Value);
+
+            httpContextAccessor.HttpContext.Response.Cookies.Append
+            (
+                cookieKey,cookieValue,options
+            );            
+        }
+
+        private void RemoveCookie(string cookieKey)
+        {
+            httpContextAccessor.HttpContext.Response.Cookies.Delete(cookieKey);
+        }
+
+
+        #endregion
+
+    }
+}
